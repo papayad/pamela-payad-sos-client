ca6b57a953fc69933bfef65963d97332fba96549
FirstName LastName <payadpamela@gmail.com>
2024-02-14 20:44:18 -0500

    add header styling for desktop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import { slide as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
 import "./Header.scss";
-import closeIcon from "../../assets/icons/close_FILL0_wght400_GRAD0_opsz24.svg";
 // replace with actual icon
 import sosLogo from "../../assets/icons/image0.webp";
 
@@ -10,29 +9,50 @@ function Header() {
     <>
       <header className="header">
         <div className="header__logo">
-          <img
-            className="header__logo-img"
-            src={sosLogo}
-            alt="sonnys on sale logo"
-          />
+          <Link to={"/"}>
+            <img
+              className="header__logo-img"
+              src={sosLogo}
+              alt="sonnys on sale logo"
+            />
+          </Link>
         </div>
         <div className="header__menu--mobile">
-          <Menu
-            className="header__burger"
-            customCrossIcon={<img src={closeIcon} />}
-            width={"18.75rem"}
-            right
-          >
+          <Menu className="header__burger" width={"18.75rem"} right>
             {/* change to icon */}
-            <Link className="header__burger-item" to={"/"}>
-              home
-            </Link>
+            <Link to={"/"}>home</Link>
             <Link to={"/signup"}>sign up</Link>
             <Link to={"/login"}>log in</Link>
             <Link to={"/createlisting"}>create listing</Link>
             <Link to={"/mylistings"}>my listings</Link>
           </Menu>
         </div>
+        <ul className="header__nav--desktop">
+          <div className="header__nav-container">
+            <li className="header__nav-item">
+              <Link className="header__nav-link" to={"/createlisting"}>
+                create listing
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <Link className="header__nav-link" to={"/mylistings"}>
+                my listings
+              </Link>
+            </li>
+          </div>
+          <div className="header__nav-container--left">
+            <li className="header__nav-item">
+              <Link className="header__nav-link--signup" to={"/signup"}>
+                sign up
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <Link className="header__nav-link" to={"/login"}>
+                log in
+              </Link>
+            </li>
+          </div>
+        </ul>
       </header>
     </>
   );
