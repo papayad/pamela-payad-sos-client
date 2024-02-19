94b17c46522b8beba96d5195818579c3ab95bc30
pamela <payadpamela@gmail.com>
2024-02-19 17:06:50 -0500

    add api calls and some edits

--- src/pages/ViewSingleListingPage/ViewSingleListingPage.js
+++ src/pages/ViewSingleListingPage/ViewSingleListingPage.js
@@ -1,7 +1,41 @@
-import React from "react";
+import "./ViewSingleListingPage.scss";
+import listingImg from "../../assets/images/peach-bear.png";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function ViewSingleListingPage() {
-  return <div>ViewSingleListingPage</div>;
+  const baseUrl = REACT_APP_SERVER_URL;
+  const { id } = useParams();
+  const [singleListingData, setSingleListingData] = useState([]);
+
+  const getSingleListingData = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8080/listings/${id}`);
+      setSingleListingData(response.data);
+    } catch (error) {
+      console.log("Error fetching listing details", error);
+    }
+  };
+
+  useEffect(() => {
+    getSingleListingData();
+  }, [id]);
+
+  return (
+    <article className="single-listing">
+      <img className="single-listing__img" src={listingImg} alt="peach bear" />
+      <div className="single-listing__details-container">
+        <h3 className="single-listing__heading">details</h3>
+        <p className="single-listing__details-text">series: {singleListingData.series}</p>
+        <p className="single-listing__details-text">name: {singleListingData.name}</p>
+        <p className="single-listing__details-text">price: </p>
+        <p className="single-listing__details-text">contact: </p>
+      </div>
+    </article>
+  );
 }
 
 export default ViewSingleListingPage;
