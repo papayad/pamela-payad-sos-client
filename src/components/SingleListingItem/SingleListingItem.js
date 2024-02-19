ae44dba346a8f7834a9d56c0641436991cd0fd4f
FirstName LastName <payadpamela@gmail.com>
2024-02-19 00:56:56 -0500

    create basic my listings page

--- src/components/SingleListingItem/SingleListingItem.js
+++ src/components/SingleListingItem/SingleListingItem.js
@@ -2,20 +2,25 @@ import "./SingleListingItem.scss";
 import listingImg from "../../assets/images/peach-bear.png";
 import openListingIcon from "../../assets/icons/chevron_right_FILL0_wght400_GRAD0_opsz24.png";
 
-function SingleListingItem() {
+function SingleListingItem({ angelName, angelSeries, listingUser }) {
   return (
     <article className="card">
-      <img className="card__img" src={listingImg} alt="peach bear sonny angel" />
+      <img
+        className="card__img"
+        src={listingImg}
+        alt="peach bear sonny angel"
+      />
       <div className="card__info-container">
         <div className="card__details-container">
-          <h2 className="card__heading">Peach Bear</h2>
+          <h2 className="card__heading">Peach Bear{angelName}</h2>
           <img
             className="card__open-icon"
             src={openListingIcon}
             alt="chevron right icon"
           />
         </div>
-        <h3 className="card__poster">listing by papayad</h3>
+        <p className="card__series">Candy Series{angelSeries}</p>
+        <h3 className="card__poster">listing by papayad{listingUser}</h3>
       </div>
       {/* link icon */}
     </article>
